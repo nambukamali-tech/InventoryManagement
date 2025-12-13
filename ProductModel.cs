96c4b4626f4b617fbbae1d94455fc8571e6c3e10
Nambu Kamali <nambukamali@gmail.com>
2025-12-13 21:51:41 +0530

    Add files via upload

---
+++ ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InventoryManagement.Models
+{
+    public class ProductModel
+    {
+        public string ProductID { get; set; }         
+        public string ProductName { get; set; }      
+        public int OpeningStock { get; set; }      
+        public int Production { get; set; }     
+        public int Dispatch { get; set; }       
+        public int ClosingStock { get; set; }       
+        public string ProductType { get; set; }        
+    }
+}
---
+++ UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagement.Models
+{
+    public class UserModel
+    {
+        
+           
+            public string FullName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string Role { get; set; }  // Admin or Employee
+        
+
+    }
+}
